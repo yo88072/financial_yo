e228b2ebe279c6124bb697198dac9cef64254169
yo88072 <62133498+yo88072@users.noreply.github.com>
2020-05-27 14:55:20 +0800

    Update models.py

--- studentsapp/models.py
+++ studentsapp/models.py
@@ -1,12 +1,13 @@
 from django.db import models
 
 class student(models.Model):
-	cName = models.CharField(max_length=20, null=False)
-	cSex = models.CharField(max_length=2, default='M', null=False)
-	cBirthday = models.DateField(null=False)
-	cEmail = models.EmailField(max_length=100, blank=True, default='')
-	cPhone = models.CharField(max_length=50, blank=True, default='')
-	cAddr = models.CharField(max_length=255,blank=True, default='')
-	
+	companyid = models.CharField(max_length=20, null=False)
+	num = models.CharField(max_length=20, default='M', null=False)
+	jobname = models.CharField(max_length=20, null=False)
+	name = models.CharField(max_length=20, null=False)
+	company = models.CharField(max_length=20, null=False)
+	cash = models.CharField(max_length=50, blank=True, default='')
+	no = models.CharField(max_length=255,blank=True, default='')
+    
 	def __str__(self):
-		return self.cName
+		return self.companyid
