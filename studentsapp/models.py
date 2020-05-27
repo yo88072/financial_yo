374b0317b067cf3ca627982663554030909948d3
yo88072 <62133498+yo88072@users.noreply.github.com>
2020-05-27 15:54:28 +0800

    Update models.py

--- studentsapp/models.py
+++ studentsapp/models.py
@@ -1,13 +1,19 @@
 from django.db import models
 
 class student(models.Model):
-	companyid = models.CharField(max_length=20, null=False)
-	num = models.CharField(max_length=20, null=False)
-	jobname = models.CharField(max_length=20, null=False)
-	name = models.CharField(max_length=20, null=False)
-	company = models.CharField(max_length=20, null=False)
-	cash = models.CharField(max_length=50, blank=True, default='')
-	no = models.CharField(max_length=255,blank=True, default='')
+	companyid = models.CharField(max_length=50, null=False)
+    
+	num = models.PositivelntegerField
+    
+	jobname =  models.CharField(max_length=50, null=False)
+    
+	name =  models.CharField(max_length=50, null=False)
+    
+	company =  models.CharField(max_length=50,blank=True)
+
+	cash =  models.PositivelntegerField
+                             
+
     
 	def __str__(self):
 		return self.companyid
